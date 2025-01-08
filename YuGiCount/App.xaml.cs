9f564e4d49a97aafa04461d9dfccc4f6ca4fa96e
Yeet195 <nils.kalinke11@gmail.com>
2025-01-08 13:26:44 +0100

    Simple notes app from microsoft tutorial

---
+++ YuGiCount/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace YuGiCount
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
